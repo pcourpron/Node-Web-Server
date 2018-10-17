d3bb9aa0bf136431e5b70a38352bc9545e82f119
pcourpron <pierrecourpron@gmail.com>
2018-10-17 14:48:19 -0700

    scrape button

--- public/app.js
+++ public/app.js
@@ -56,6 +56,56 @@ $(document).on("click", "#savenote", function() {
   $("#bodyinput").val("");
 });
 
+$(document).on('click','#scrape',function(){
+  var results = []
+  axios.get("https://www.dissentmagazine.org/").then(function (response) {
+
+      // Then, we load that into cheerio and save it to $ for a shorthand selector
+      var $ = cheerio.load(response.data);
+
+
+      // Now, we grab every h2 within an article tag, and do the following:
+      $("article").each(function (i, element) {
+
+          // Save an empty result object
+          var result = {};
+
+          // Add the text and href of every link, and save them as properties of the result object
+          result.title = $(this)
+              .children('header')
+              .children("h3")
+              .text();
+          result.link = $(this)
+              .children('header')
+              .children("h3")
+              .children('a')
+              .attr("href");
+          result.body= $(this)
+              .children('p')
+              .text();
+
+
+          results.push(result)
+          console.log(result)
+          // Create a new Article using the `result` object built from scraping
+          db.Article.create(result)
+              .then(function (dbArticle) {
+                  // View the added result in the console
+                  console.log(dbArticle);
+              })
+              .catch(function (err) {
+
+              });
+      });
+      console.log(results)
+      res.render('view', {
+          results: results
+      }
+      )
+
+  });
+})
+
 $(document).on("click", "#deleteComments", function() {
   // Grab the id associated with the article from the submit button
   var thisId = $(this).attr("data-id");
--- routes/HTMLroutes.js
+++ routes/HTMLroutes.js
@@ -4,7 +4,7 @@ var axios = require('axios');
 var cheerio = require('cheerio')
 
 module.exports = function (app) {
-    app.get('/view', (req, res) => {
+    app.get('/', (req, res) => {
 
         db.Article.find().then(function(all){
             console.log('==========================================================================================')
@@ -15,56 +15,7 @@ module.exports = function (app) {
             )
             console.log('=========================================================================================================')
         })
-        /*
-        var results = []
-        axios.get("https://www.dissentmagazine.org/").then(function (response) {
-
-            // Then, we load that into cheerio and save it to $ for a shorthand selector
-            var $ = cheerio.load(response.data);
-
-
-            // Now, we grab every h2 within an article tag, and do the following:
-            $("article").each(function (i, element) {
-
-                // Save an empty result object
-                var result = {};
-
-                // Add the text and href of every link, and save them as properties of the result object
-                result.title = $(this)
-                    .children('header')
-                    .children("h3")
-                    .text();
-                result.link = $(this)
-                    .children('header')
-                    .children("h3")
-                    .children('a')
-                    .attr("href");
-                result.body= $(this)
-                    .children('p')
-                    .text();
-
-
-                results.push(result)
-                console.log(result)
-                // Create a new Article using the `result` object built from scraping
-                db.Article.create(result)
-                    .then(function (dbArticle) {
-                        // View the added result in the console
-                        console.log(dbArticle);
-                    })
-                    .catch(function (err) {
-
-                    });
-            });
-            console.log(results)
-            res.render('view', {
-                results: results
-            }
-            )
-
-        });
-
-*/
+       
 
     })
     app.get('/landing', (req, res) => {
